56e88a68db0fc88cf702e85b08ec5f578f5e3101
__RD <beantang.tang@gmail.com>
2014-12-28 14:59:32 +0800

    cf301c

---
+++ cf301c/algorithm.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+int main()
+{
+	puts("9??>>??0");
+	for (int i=0;i<9;i++) printf("%d??<>%d\n",i,i+1);
+	puts("??<>1");
+	for (int i=0;i<=9;i++) printf("?%d>>%d?\n",i,i);
+	puts("?>>??");
+	puts(">>?");
+}
+
